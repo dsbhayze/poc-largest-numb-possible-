51ebaa3baffa81f5c056072661d31df1c2364468
Rafael <rafael.farias.ext@spms.min-saude.pt>
2022-07-29 22:11:51 +0100

    Add test cases

--- src/main/java/com/poc/Main.java
+++ src/main/java/com/poc/Main.java
@@ -1,9 +1,15 @@
 package com.poc;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Main {
 
@@ -36,5 +42,12 @@ public class Main {
     private String printHigherCombination() {
         //TODO
         return null;
+    }
+
+    @Test
+    public void testCase() {
+        assertEquals("95021", giveHigherCombination(Stream.of(50, 2, 1, 9).collect(Collectors.toList())));
+        //assertEquals("56550", giveHigherCombination(Stream.of(5,50,56).collect(Collectors.toList())));
+        //assertEquals("42423420", giveHigherCombination(Stream.of(420,42,423).collect(Collectors.toList())));
     }
 }
